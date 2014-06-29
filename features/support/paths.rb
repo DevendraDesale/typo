a7b9666f87b659a1aeca09f05c5b4e700e49e3d6
DevendraDesale <devendra.desale@gmail.com>
2014-06-29 14:39:37 +0530

    Compliting the background setup for the merge article feature.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -17,8 +17,9 @@ module NavigationHelpers
       '/'
     when /^the new article page$/
       '/admin/content/new'
-    when /^the admin new category page$/
-      '/admin/categories/new'
+    when /^the Edit Page of Article with id (\d+)$/
+      "/admin/content/edit/#{$1}"
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
